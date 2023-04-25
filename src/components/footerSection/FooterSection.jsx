86b1333444f2aa6df1407deddd733d4accf32666
fvangelista <bortoletti.filippo95@gmail.com>
2023-04-25 21:35:38 +0200

    feat: icons

--- src/components/footerSection/FooterSection.jsx
+++ src/components/footerSection/FooterSection.jsx
@@ -1,17 +1,27 @@
+import { RiGithubLine, RiLinkedinLine } from "react-icons/ri";
 import styles from "./index.module.scss";
-
 import logo from "../../assets/logo.svg";
 
 function FooterSection({ refs }) {
   return (
     <div ref={refs.footerRef} className={styles.Footer}>
-      <div className={styles.info}>
-        <span>&copy; Cocktails</span>
-        <span>E-mail: contact@contact.com</span>
-      </div>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
       </div>
+      <div className={styles.info}>
+        <span>&copy; Cocktails - by filippo</span>
+      </div>
+      <ul>
+        <a className={styles.icon} href="https://github.com/FVangelista">
+          <RiGithubLine />
+        </a>
+        <a
+          className={styles.icon}
+          href="https://linkedin.com/in/vangelistafilippo"
+        >
+          <RiLinkedinLine />
+        </a>
+      </ul>
     </div>
   );
 }
